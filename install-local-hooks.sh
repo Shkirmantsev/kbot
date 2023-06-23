1330e9fc5b461757dab2ea0c31eb0e6075e1e757
Dimitri Schkirmanzew <shkirmantsev@gmail.com>
2023-06-23 12:58:23 +0200

    Implement local git hooks installation
    The reason of using docker - multi platform support.
    See [1],[2],[3], [4]
    
    [1]: https://github.com/gitleaks/gitleaks
    [2]: https://pre-commit.com/hooks
    [3]: https://pre-commit.com/#install
    [4]: https://github.com/gitleaks/gitleaks/blob/master/.pre-commit-hooks.yaml

---
+++ install-local-hooks.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Pull the gitleaks docker image
+docker pull zricethezav/gitleaks
+
+# Run the pre-commit install inside a docker container
+docker run -it --rm -v "$(pwd)":/src -w /src python:3.10.12 bash -c "pip install pre-commit && pre-commit install"
