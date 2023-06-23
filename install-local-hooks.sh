54086f75fd760c0964f7e81b0274371e0b7704c4
Dimitri Schkirmanzew <shkirmantsev@gmail.com>
2023-06-23 13:12:29 +0200

    Try to fix installation error
    Currently, during installation, the following error occurs:
    "the input device is not a TTY"

--- install-local-hooks.sh
+++ install-local-hooks.sh
@@ -4,4 +4,4 @@
 docker pull zricethezav/gitleaks
 
 # Run the pre-commit install inside a docker container
-docker run -it --rm -v "$(pwd)":/src -w /src python:3.10.12 bash -c "pip install pre-commit && pre-commit install"
+docker run --rm -v "$(pwd)":/src -w /src python:3.10.12 bash -c "pip install pre-commit && pre-commit install"
